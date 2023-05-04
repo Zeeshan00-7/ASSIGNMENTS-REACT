ba117737a39edd3b857c6efb1a9fb3373757087d
Zeeshan00-7 <za753073@gmail.com>
2023-05-04 16:55:33 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import OtpGenerate from './component/otp';
+import React from 'react';
+import './App.css'
+import { useRef } from 'react';
 
-function App() {
+export default function App() {
+  const apiUrl = 'https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP';
+
+  const inputref = useRef(' ')
+
+  function mobileOtp() {
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        mobile:parseInt(inputref.current.value),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => console.error(error));
+  }
   return (
-    <div className="App">
-
-    <OtpGenerate/>
-
+    <div>
+      <input type="number" ref={inputref} placeholder='number' />
+      <button onClick={mobileOtp}>OTP</button>
     </div>
   );
 }
-
-export default App;
